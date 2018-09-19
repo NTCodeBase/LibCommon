a8e8d059168192753eb3d5cdbcc92b9ae62c3c04
Nghia Truong <nghiatruong.vn@gmail.com>
2018-09-19 01:01:58 -0600

    :golf: add function to convert between quaternion and vec4

--- LibCommon/LinearAlgebra/LinaHelpers.h
+++ LibCommon/LinearAlgebra/LinaHelpers.h
@@ -462,4 +462,8 @@ auto quaternionMatrixTransposed3x4(const Quaternion& q)
 }
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+template<class T> Vec4<T> quatToVec4(const Quat<T>& q) { return Vec4<T>(q.x, q.y, q.z, q.w); }
+template<class T> Quat<T> vec4ToQuat(const Vec4<T>& q) { return Quat<T>(q.w, q.x, q.y, q.z); }
+
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 }   // end namespace LinaHelpers
