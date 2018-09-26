13c459d30e982ffca12fcfee383e62db471e56e5
Nghia Truong <nghiatruong.vn@gmail.com>
2018-09-26 10:38:47 -0600

    :hamburger: rewrite atomic operations

--- LibCommon/ParallelHelpers/AtomicOperations.h
+++ LibCommon/ParallelHelpers/AtomicOperations.h
@@ -22,9 +22,8 @@ namespace AtomicOperations
 {
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 template<class T, class Function>
-void atomicOp(T& target, const T& operand, Function&& f)
+__NT_FORCE_INLINE void atomicOp(T& target, const T& operand, Function&& f)
 {
-    static_assert(sizeof(T) <= 8);
     std::atomic<T>& tgt = *((std::atomic<T>*)&target);
 
     T cur_val = target;
@@ -36,61 +35,77 @@ void atomicOp(T& target, const T& operand, Function&& f)
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 template<class T>
-void add(T& target, const T& operand)
+__NT_FORCE_INLINE void add(T& target, const T& operand)
 {
     atomicOp(target, operand, [](T a, T b) { return a + b; });
 }
 
 template<Int N, class T>
-void add(VecX<N, T>& target, const VecX<N, T>& operand)
+__NT_FORCE_INLINE void add(VecX<N, T>& target, const VecX<N, T>& operand)
 {
-    for(Int d = 0; d < N; ++d) {
-        atomicOp(target[d], operand[d], [](T a, T b) { return a + b; });
+    if constexpr (N == 4) {
+        atomicOp(target, operand, [](T a, T b) { return a + b; });
+    } else {
+        for(Int d = 0; d < N; ++d) {
+            atomicOp(target[d], operand[d], [](T a, T b) { return a + b; });
+        }
     }
 }
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 template<class T>
-void subtract(T& target, const T& operand)
+__NT_FORCE_INLINE void subtract(T& target, const T& operand)
 {
     atomicOp(target, operand, [](T a, T b) { return a - b; });
 }
 
 template<Int N, class T>
-void subtract(VecX<N, T>& target, const VecX<N, T>& operand)
+__NT_FORCE_INLINE void subtract(VecX<N, T>& target, const VecX<N, T>& operand)
 {
-    for(Int d = 0; d < N; ++d) {
-        atomicOp(target[d], operand[d], [](T a, T b) { return a - b; });
+    if constexpr (N == 4) {
+        atomicOp(target, operand, [](T a, T b) { return a + b; });
+    } else {
+        for(Int d = 0; d < N; ++d) {
+            atomicOp(target[d], operand[d], [](T a, T b) { return a - b; });
+        }
     }
 }
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 template<class T>
-void multiply(T& target, const T& operand)
+__NT_FORCE_INLINE void multiply(T& target, const T& operand)
 {
     atomicOp(target, operand, [](T a, T b) { return a * b; });
 }
 
 template<Int N, class T>
-void multiply(VecX<N, T>& target, const VecX<N, T>& operand)
+__NT_FORCE_INLINE void multiply(VecX<N, T>& target, const VecX<N, T>& operand)
 {
-    for(Int d = 0; d < N; ++d) {
-        atomicOp(target[d], operand[d], [](T a, T b) { return a * b; });
+    if constexpr (N == 4) {
+        atomicOp(target, operand, [](T a, T b) { return a + b; });
+    } else {
+        for(Int d = 0; d < N; ++d) {
+            atomicOp(target[d], operand[d], [](T a, T b) { return a * b; });
+        }
     }
 }
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 template<class T>
-void divide(T& target, const T& operand)
+__NT_FORCE_INLINE void divide(T& target, const T& operand)
 {
     atomicOp(target, operand, [](T a, T b) { return a / b; });
 }
 
 template<Int N, class T>
-void divide(VecX<N, T>& target, const VecX<N, T>& operand)
+__NT_FORCE_INLINE void divide(VecX<N, T>& target, const VecX<N, T>& operand)
 {
-    for(Int d = 0; d < N; ++d) {
-        atomicOp(target[d], operand[d], [](T a, T b) { return a / b; });
+    if constexpr (N == 4) {
+        atomicOp(target, operand, [](T a, T b) { return a + b; });
+    } else {
+        for(Int d = 0; d < N; ++d) {
+            atomicOp(target[d], operand[d], [](T a, T b) { return a / b; });
+        }
     }
 }
 
