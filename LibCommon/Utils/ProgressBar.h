15d6f00b0e2072b346017ad484bf7a26087569c2
Nghia Truong <nghiatruong.vn@gmail.com>
2018-10-24 23:24:37 -0600

    implement progressbar

---
+++ LibCommon/Utils/ProgressBar.h
@@ -0,0 +1,43 @@
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//
+//    .--------------------------------------------------.
+//    |  This file is part of NTCodeBase                 |
+//    |  Created 2018 by NT (https://ttnghia.github.io)  |
+//    '--------------------------------------------------'
+//                            \o/
+//                             |
+//                            / |
+//
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+#pragma once
+
+#include <LibCommon/CommonSetup.h>
+
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+template<UInt BarWidth, class Real_t = float, char PrefixPadding = '=', char SuffixPadding = ' '>
+class ProgressBar {
+public:
+    ProgressBar() = delete;
+    static String getProgressBar(Real_t percentage) {
+        assert(percentage > Real_t(0) && percentage < Real_t(100.0));
+        return m_ProgressBars[static_cast<UInt>(percentage)];
+    }
+
+private:
+    constexpr static StdVT_String generateProgressBars() {
+        StdVT_String bars(101u);
+        for(UInt i = 0; i < 101u; ++i) {
+            auto lpad = static_cast<UInt>(std::round(i * BarWidth / 100.0));
+            auto rpad = BarWidth - lpad;
+            bars[i] = String("[") + String(lpad, PrefixPadding) + String(">") + String(rpad, SuffixPadding) + String("]");
+        }
+        return bars;
+    }
+
+    static inline StdVT_String m_ProgressBars = generateProgressBars();
+};
+
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
