435f5df666b0ed0d41707dc754c6db906d2eb1a9
Nghia Truong <nghiatruong.vn@gmail.com>
2019-03-05 11:58:09 -0700

    fix geometry factory

--- LibCommon/Geometry/GeometryObjectFactory.cpp
+++ LibCommon/Geometry/GeometryObjectFactory.cpp
@@ -33,11 +33,37 @@ bool GeometryObjectFactory<N, Real_t>::registerGeometry(const String& geometryNa
 template<Int N, class Real_t>
 void GeometryObjectFactory<N, Real_t>::registerGeometries() {
     __NT_REQUIRE(registerGeometry(BoxObject<N, Real_t>::name(), BoxObject<N, Real_t>::createGeometry));
+    __NT_REQUIRE(registerGeometry(SphereObject<N, Real_t>::name(), SphereObject<N, Real_t>::createGeometry));
+    __NT_REQUIRE(registerGeometry(TorusObject<N, Real_t>::name(), TorusObject<N, Real_t>::createGeometry));
+    __NT_REQUIRE(registerGeometry(Torus28Object<N, Real_t>::name(), Torus28Object<N, Real_t>::createGeometry));
+    __NT_REQUIRE(registerGeometry(Torus2InfObject<N, Real_t>::name(), Torus2InfObject<N, Real_t>::createGeometry));
+    __NT_REQUIRE(registerGeometry(Torus88Object<N, Real_t>::name(), Torus88Object<N, Real_t>::createGeometry));
+    __NT_REQUIRE(registerGeometry(TorusInfInfObject<N, Real_t>::name(), TorusInfInfObject<N, Real_t>::createGeometry));
+    __NT_REQUIRE(registerGeometry(CylinderObject<N, Real_t>::name(), CylinderObject<N, Real_t>::createGeometry));
+    __NT_REQUIRE(registerGeometry(ConeObject<N, Real_t>::name(), ConeObject<N, Real_t>::createGeometry));
+    __NT_REQUIRE(registerGeometry(CapsuleObject<N, Real_t>::name(), CapsuleObject<N, Real_t>::createGeometry));
+    __NT_REQUIRE(registerGeometry(EllipsoidObject<N, Real_t>::name(), EllipsoidObject<N, Real_t>::createGeometry));
+    __NT_REQUIRE(registerGeometry(CSGObject<N, Real_t>::name(), CSGObject<N, Real_t>::createGeometry));
+
+    if constexpr(N == 2) {
+        __NT_REQUIRE(registerGeometry(PlaneObject<N, Real_t>::name(), PlaneObject<N, Real_t>::createGeometry));
+        __NT_REQUIRE(registerGeometry(TriangleObject<N, Real_t>::name(), TriangleObject<N, Real_t>::createGeometry));
+        __NT_REQUIRE(registerGeometry(HexagonObject<N, Real_t>::name(), HexagonObject<N, Real_t>::createGeometry));
+    } else {
+        __NT_REQUIRE(registerGeometry(TriangularPrismObject<N, Real_t>::name(), TriangularPrismObject<N, Real_t>::createGeometry));
+        __NT_REQUIRE(registerGeometry(HexagonalPrismObject<N, Real_t>::name(), HexagonalPrismObject<N, Real_t>::createGeometry));
+        __NT_REQUIRE(registerGeometry(TriMeshObject<N, Real_t>::name(), TriMeshObject<N, Real_t>::createGeometry));
+    }
 }
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 template<Int N, class Real_t>
 SharedPtr<GeometryObject<N, Real_t>> GeometryObjectFactory<N, Real_t>::createGeometry(const String& geometryName, const JParams& jParams) {
+    if(!s_bInitialized) {
+        registerGeometries();
+        s_bInitialized = true;
+    }
+    ////////////////////////////////////////////////////////////////////////////////
     auto& creationFuncs = getCreationFuncPtrs();
     __NT_REQUIRE(creationFuncs.find(geometryName) != creationFuncs.end());
     auto geoObj = creationFuncs.at(geometryName)(jParams); // call the creationFunc
